cd800d742a0561b330c9b16f2fc10bde5b3142c5
Ulug`bek <avazov0989@gmail.com>
2021-01-24 15:19:44 +0500

    '6'

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
-import React, {  } from "react";
+import React from "react";
+import Typewriter from "typewriter-effect";
 
 export const Home = () => {
-
   return (
     <div className="home i-c">
-      <h1 className="t-c">Hello my name is Avazov Ulug`bek</h1>
+      <h1 className="t-c">
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString("Hello my name is Avazov Ulug`bek!")
+              .pauseFor(2500)
+              .start();
+          }}
+        />
+      </h1>
     </div>
   );
 };
